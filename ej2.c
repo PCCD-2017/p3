aa7a6186347bef2485c68d1a405bbdabc1a8f23a
David Rodriguez Pereira <mxmax95@gmail.com>
2017-01-31 18:43:20 +0100

    Exercise 2 done.

--- ej2.c
+++ ej2.c
@@ -2,7 +2,26 @@
  * Ejercicio 2
  */
 
-int main(int argc, char* argv[]){
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <printf.h>
 
+int main(int argc, char* argv[]){
+    int shmid;
+    key_t key = 88888;
+    int* dir_memCompartida;
+
+
+    if ((shmid = shmget(key, NULL, 0)) == -1) {
+        perror("Hubo un error al ejecutar shmget.");
+        exit(1);
+    } else {
+        if ((dir_memCompartida = shmat(shmid, NULL, 0)) == -1) {
+            perror("Shmat ha encontrado un error.");
+            exit(1);
+        } else {
+            printf("El valor encontrado en la dirección de memoria es %d \n.", *dir_memCompartida);
+        }
+    }
     return 0;
 }
