9444d3b755664c7436d97bcb239b568d13bdbdb1
David Rodriguez Pereira <mxmax95@gmail.com>
2017-01-31 22:57:21 +0100

    Exercise 3 fully working.
    Added failure protection.

--- p1_proc.c
+++ p1_proc.c
@@ -7,9 +7,10 @@
 #include <sys/shm.h>
 #include <stdlib.h>
 #include <printf.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
-    int shmid;
+    int shmid, err;
     key_t key = 88888;
     int *dir_memCompartida;
     int stop;
@@ -27,11 +28,16 @@ int main(int argc, char *argv[]) {
                 printf("Se ha escrito en memoria compartida el valor %d .\n", *dir_memCompartida);
                 printf("Pulse 0 para salir u otro entero para continuar.\n");
                 fscanf(stdin, "%d", &stop);
+                if(shmget(key, NULL, 0) == -1 ) {
+                    err = errno;
+                    break;
+                }
                 if (stop != 0) {
                     *dir_memCompartida = stop;
                 }
             } while (stop != 0);
             printf("Deteniendo escritura.\n");
+            if(err == ENOENT) printf("No existe un segmento de memoria compartida con la clave dada.\n");
             shmdt((char *) dir_memCompartida);
         }
         exit(0);
--- p2_proc.c
+++ p2_proc.c
@@ -5,9 +5,10 @@
 #include <sys/shm.h>
 #include <stdlib.h>
 #include <printf.h>
+#include <errno.h>
 
 int main(int argc, char* argv[]){
-    int shmid;
+    int shmid, err;
     key_t key = 88888;
     int* dir_memCompartida;
     int stop;
@@ -19,13 +20,17 @@ int main(int argc, char* argv[]){
             perror("Shmat ha encontrado un error.");
             exit(1);
         } else {
-            //TODO añadir bucle que persista leyendo mientras no se pulse una tecla.
             do{
                 system("clear");
                 printf("El dato en memoria compartida es %d \n", *dir_memCompartida);
                 printf("Pulse 0 para salir u otro entero para continuar.\n");
+                if(shmget(key, NULL, 0) == -1 ) {
+                    err = errno;
+                    break;
+                }
                 fscanf(stdin,"%d",&stop);
             }while(stop!=0);
+            if(err == ENOENT) printf("No existe un segmento de memoria compartida con la clave dada.\n");
             printf("Saliendo.\n");
             shmdt((char *)dir_memCompartida);
         }
