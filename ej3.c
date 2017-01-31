fae3d7aa6b140cf073125b15a488e59ef9fd53f1
David Rodriguez Pereira <mxmax95@gmail.com>
2017-01-31 18:55:18 +0100

    Duplicated p1 and p2.

--- ej3.c
+++ ej3.c
@@ -3,6 +3,7 @@
  */
 
 int main(int argc, char* argv[]){
+    //TODO escribir proceso que libere zona de memoria compartida
 
     return 0;
 }
---
+++ p2_proc.c
@@ -0,0 +1,28 @@
+/*
+ * Ejercicio 2
+ */
+
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <printf.h>
+
+int main(int argc, char* argv[]){
+    int shmid;
+    key_t key = 88888;
+    int* dir_memCompartida;
+
+
+    if ((shmid = shmget(key, NULL, 0)) == -1) {
+        perror("Hubo un error al ejecutar shmget.");
+        exit(1);
+    } else {
+        if ((dir_memCompartida = shmat(shmid, NULL, 0)) == -1) {
+            perror("Shmat ha encontrado un error.");
+            exit(1);
+        } else {
+            //TODO añadir bucle que persista leyendo mientras no se pulse una tecla.
+            printf("El valor encontrado en la dirección de memoria es %d \n.", *dir_memCompartida);
+        }
+    }
+    return 0;
+}
